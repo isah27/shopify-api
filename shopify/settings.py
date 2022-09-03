95d9bc2005fcf926ee42eae219ab701e80057ba0
isah27 <isahnaziru27@gmail.com>
2022-09-03 21:10:13 +0100

    fix error

--- shopify/settings.py
+++ shopify/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT= BASE_DIR / 'staticfiles'
+STATIC_ROOT= BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
