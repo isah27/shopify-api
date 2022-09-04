f7e49391c731f13a74a5362a62b9992befe7af25
isah27 <isahnaziru27@gmail.com>
2022-09-04 12:56:23 +0100

    fix error

--- shopify/settings.py
+++ shopify/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.utils.translation',
+    # 'django.utils.translation',
     
     'authentication.apps.AuthenticationConfig',
     'products.apps.ProductsConfig',
